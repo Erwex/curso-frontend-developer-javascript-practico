36e282101f0c25f528f846dedf532717e1ce2715
Enrique Loaiza <enriqueloaiza48@gmail.com>
2022-08-08 20:19:26 -0400

    actualizacion del menu conectando con JS

--- main.js
+++ main.js
@@ -1,9 +1,46 @@
 const menuEmail = document.querySelector('.navbar-email');
-const DesktopMenu = document.querySelector('.desktop-menu')
+const DesktopMenu = document.querySelector('.desktop-menu');
+const burguerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
+const menuCarritoIcon = document.querySelector ('.navbar-shopping-cart');
+
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
+burguerMenu.addEventListener('click', toggleMobileMenu)
+menuCarritoIcon.addEventListener('click', toggleCarritoAside)
+
+
+
 
 function toggleDesktopMenu() {
+    const isAsideOpen = !aside.classList.contains('inactive')
+    
+    if (isAsideOpen) {
+        aside.classList.add('inactive')
+    }
+
 DesktopMenu.classList.toggle('inactive');
 }
-console.log('funcionando')
+
+function toggleMobileMenu() {
+    const isAsideOpen = !aside.classList.contains('inactive')
+    
+    if (isAsideOpen) {
+        aside.classList.add('inactive')
+    }
+mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+    const ismobileMenuOpen = !mobileMenu.classList.contains('inactive')
+    
+
+    if (ismobileMenuOpen) {
+        mobileMenu.classList.add('inactive');
+    }
+    
+    aside.classList.toggle('inactive');     
+
+}
